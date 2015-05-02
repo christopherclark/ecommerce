9da8cb433f22f568f52a4561d4f2282f65d2e0c4
Paula Chojnacki <chojnacki.paula@gmail.com>
2015-05-02 16:38:05 -0700

    added methods to controllers and models

--- application/controllers/purchases.php
+++ application/controllers/purchases.php
@@ -5,11 +5,42 @@ class  Purchases extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->output->enable_profiler();
+		$this->load->model('purchase');
 	}
 
 	public function index()
 	{
-		echo "Welcome to CodeIgniter. The default Controller is purchases.php";
+		$data['products'] = $this->Purchase->get_all_products();
+		$data['categories'] = $this->Purchase->get_all_categories();
+		$this->load->view('/purchases/all_products', $data);
+	}
+
+	public function get_products_by_category()
+	{
+		$data['page_no'] = $this->Purchase->count($this->input->post());
+		$data['products'] = $this->Purchase->get_products_by_category($this->input->post());
+		$this->load->view('/purchases/all_products', $data);
+	}
+
+	public function get_product_by_id()
+	{
+		$data['product'] = $this->Purchase->get_product_by_id($id);
+		$this->load->view('/purchases/view_product', $data);
+	}
+
+	public function add_to_cart()
+	{
+
+	}
+
+	public function view_cart()
+	{
+		$this->load->view('/purchases/checkout', $data);
+	}
+
+	public function validate_billing()
+	{
+
 	}
 }
+
