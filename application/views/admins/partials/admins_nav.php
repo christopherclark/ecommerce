b74b0af00b29951d9f3840dfb342b444a4e5bd4b
Rod Woodman <roderickwoodman@gmail.com>
2015-05-02 17:25:53 -0700

    Added partials for navbar and <head> for views.

---
+++ application/views/admins/partials/admins_nav.php
@@ -0,0 +1,14 @@
+<nav class="admins navbar navbar-default navbar-fixed-top">
+	<div class="container-fluid">
+		<ul class="nav navbar-header">
+			<li class="navbar-brand">Dashboard</li>
+		</ul>
+		<ul class="nav navbar-nav navbar-left">
+			<li><a class="active" href="/admins/get_all_orders">Orders</a></li>
+			<li><a href="/admins/get_all_products">Products</a></li>
+		</ul>
+		<ul class="nav navbar-nav navbar-right">
+			<li><a href="/admins/logoff">log off</a></li>
+		</div>
+	</div>
+</nav>
---
+++ application/views/purchases/partials/purchases_nav.php
@@ -0,0 +1,16 @@
+<nav class="purchases navbar navbar-default navbar-fixed-top">
+	<div class="container-fluid">
+		<ul class="nav navbar-header">
+			<p class="navbar-brand">H&H Supplies</p>
+		</ul>
+		<ul class="nav navbar-nav navbar-right">
+			<li><a href="/purchases/view_cart">Shopping Cart (<?php
+					if(FALSE !== ($this->session->userdata('cart_count'))) {
+						echo $this->session->userdata('cart_count');
+					} else {
+						echo "0";
+					}
+					?>)</a></li>
+		</ul>
+	</div>
+</nav>
