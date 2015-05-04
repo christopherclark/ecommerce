0e391c8d1c8a707daf686fa7dd98195a0d860164
Chris Clark <chrisclark1979@gmail.com>
2015-05-04 13:15:32 -0700

    battling the controller

--- application/controllers/purchases.php
+++ application/controllers/purchases.php
@@ -22,15 +22,54 @@ class  Purchases extends CI_Controller {
 		$this->load->view('/purchases/all_products', $data);
 	}
 
-	public function get_product_by_id()
+	public function get_product_by_id($id)
 	{
 		$data['product'] = $this->Purchase->get_product_by_id($id);
 		$this->load->view('/purchases/view_product', $data);
 	}
 
-	public function add_to_cart()
+	public function add_cart_item()
 	{
+	
+		if(!$this->session->userdata($cart_items[$this->input->post['id']]))
+	 		{
+			$this->session->set_userdata($cart_items[$this->input->post['id']], $this->input->post['quantity']);
+			}
+		else
+			{
+			$this->session->set_userdata($cart_items[$this->input->post['id']],
+			 $this->session->userdata($cart_items[$this->input->post['id']]) + $this->input->post['quantity']);
+			}
 		
+		foreach($this->session->userdata($cart_items) as $cart_item => $quantity)
+			{
+				$total_quantity += $quantity;
+			}
+		
+
+		$this->session->set_userdata("total_quantity", $total_quantity);
+ /// 1. update session["cart"]   [{"product"=>"1","qty"=>"3"},...] with post[id] and post[qty]
+		
+
+// 2. get products.id products.name and products.price from DB for all id's in cart => "cart_inventory"
+
+		// $cart_inventory = array(
+			
+		// 	"id" => $id;
+		// 	"product"=> $data['name'],
+		// 	"price" => $data['price'],
+		// 	"quantity" => $quantity;
+		// 	"sub_total" => $data['price'] * $quantity;
+
+
+		// 	"total_quantity" => //add all quantities
+		// 	"total_price" =>	//add all sub_total
+		// 	)		
+
+		// $this->Purchase->get_product_by_id($id)
+	
+		var_dump($this->session->userdata());
+		die();
 	}
 
 	public function view_cart()
